22b2a68a963317d141efebbf27f81f6cea2daa34
Chris Evans <Cevans@peerlessproducts.com>
2018-10-08 18:12:35 -0500

    worked on the formatting of the main page.  added the buttons in a listview and are provided by the viewmodel

--- EvansBioApp/EvansBioApp/ViewModels/MainPageViewModel.cs
+++ EvansBioApp/EvansBioApp/ViewModels/MainPageViewModel.cs
@@ -3,22 +3,53 @@ using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using DelegateCommand = Prism.Commands.DelegateCommand;
 
 namespace EvansBioApp.ViewModels
 {
+    public class MainPageButton : BindableBase
+    {
+        public MainPageButton(DelegateCommand cmd, string title, string subTitle)
+        {
+            Command = cmd;
+            Title = title;
+            SubTitle = subTitle;
+        }
+        public DelegateCommand Command { get;  }
+        public string Title { get;  }
+
+        public string SubTitle { get; }
+    }
     public class MainPageViewModel : ViewModelBase
     {
+        private MainPageButton _selectedButton;
+
         public MainPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = "Main Page";
-            AboutPageCommand = new DelegateCommand(async ()=> await NavigationService.NavigateAsync("AboutPage"));
-            SkillsPageCommand = new DelegateCommand(async ()=> await NavigationService.NavigateAsync("SkillsPage"));
-            ContactMePageCommand = new DelegateCommand(async ()=> await NavigationService.NavigateAsync("ContactMePage"));
+            AboutPageCommand = new DelegateCommand(async () => await NavigationService.NavigateAsync("AboutPage"));
+            SkillsPageCommand = new DelegateCommand(async () => await NavigationService.NavigateAsync("SkillsPage"));
+            ContactMePageCommand =
+                new DelegateCommand(async () => await NavigationService.NavigateAsync("ContactMePage"));
+            DelegateCommands = new ObservableCollection<MainPageButton>()
+            {
+                new MainPageButton(AboutPageCommand, "About Me", "Who I am and what I do"),
+                new MainPageButton(SkillsPageCommand, "My Skills", "My stenghts and proficiencies"),
+                new MainPageButton(ContactMePageCommand, "Contact Me", "Need more information?  Let me know!")
+            };
         }
 
+        public MainPageButton SelectedButton
+        {
+            get => _selectedButton;
+            set { SetProperty(ref _selectedButton, value, () => SelectedButton?.Command.Execute()); }
+        }
+
+        public ObservableCollection<MainPageButton> DelegateCommands { get; set; }
         public DelegateCommand ContactMePageCommand { get; set; }
         public DelegateCommand SkillsPageCommand { get; set; }
         public DelegateCommand AboutPageCommand { get; set; }
