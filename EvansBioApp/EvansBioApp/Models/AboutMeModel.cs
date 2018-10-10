7e2ab0e04f80d62267c76b151d83986d27ce1f9a
Chris Evans <Cevans@peerlessproducts.com>
2018-10-10 17:59:49 -0500

    added image assets

---
+++ EvansBioApp/EvansBioApp/Models/AboutMeModel.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+
+namespace EvansBioApp.Models
+{
+    public class AboutMeModel
+    {
+        public string Paragraph { get; set; }
+
+        public AboutMeModel()
+        {
+            Paragraph = File.ReadAllText("./Assets/AboutMe.txt");
+        }
+    }
+}
--- EvansBioApp/EvansBioApp/Models/SkillsModel.cs
+++ EvansBioApp/EvansBioApp/Models/SkillsModel.cs
@@ -1,7 +1,15 @@
 ﻿namespace EvansBioApp.Models
 {
-    public class SkillsModel
-    {
-        
-    }
+    //public class SkillsModel
+    //{
+    //    public string ProgrammingLanguages { get; set; }
+    //    public string Frameworks { get; set; }
+    //    public string Software { get; set; }
+    //    public string OtherSkills { get; set; }
+
+    //    public SkillsModel()
+    //    {
+    //    }
+
+    //}
 }
