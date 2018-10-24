5909f73a61a3689e75a5aa2aff7c8abbf56850d1
Chris Evans <Cevans@peerlessproducts.com>
2018-10-24 17:54:06 -0500

    finished contact me page

--- EvansBioApp/EvansBioApp/ViewModels/ContactMePageViewModel.cs
+++ EvansBioApp/EvansBioApp/ViewModels/ContactMePageViewModel.cs
@@ -3,14 +3,57 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Prism.Services;
 
 namespace EvansBioApp.ViewModels
 {
 	public class ContactMePageViewModel : BindableBase
 	{
-        public ContactMePageViewModel()
-        {
+	    private readonly IPageDialogService _pageDialog;
+	    private string _details;
+	    private string _subject;
+	    private string _email;
 
-        }
+	    public ContactMePageViewModel(IPageDialogService pageDialog)
+	    {
+	        _pageDialog = pageDialog;
+	        SendCommand = new DelegateCommand(Send, CanSend);
+	        SendCommand.ObservesProperty(() => Email);
+	        SendCommand.ObservesProperty(() => Subject);
+	        SendCommand.ObservesProperty(() => Details);
+	    }
+
+	    private bool CanSend()
+	    {
+	        return !string.IsNullOrWhiteSpace(Email) && !string.IsNullOrWhiteSpace(Details) &&
+	               !string.IsNullOrWhiteSpace(Subject);
+	    }
+
+	    public string Email
+	    {
+	        get => _email;
+	        set => SetProperty(ref _email,value);
+	    }
+
+	    public string Details
+	    {
+	        get => _details;
+	        set => SetProperty(ref _details,value);
+	    }
+
+	    public string Subject
+	    {
+	        get => _subject;
+	        set => SetProperty(ref _subject,value);
+	    }
+
+	    private async void Send()
+	    {
+	        await _pageDialog.DisplayAlertAsync("Success!",
+	            "Thank you for your message!\nWe have received your message and will reply as soon as we can.", "OK");
+
+	    }
+
+	    public DelegateCommand SendCommand { get; set; }
 	}
 }
