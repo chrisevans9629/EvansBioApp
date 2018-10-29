00c1d06c49ea6bb6d5f7c857d208109da55c6c0b
Chris Evans <Cevans@peerlessproducts.com>
2018-10-29 17:30:03 -0500

    adjusted skills page, fixed image not showing up, added navigate back when the send button is pressed.

--- EvansBioApp/EvansBioApp/ViewModels/ContactMePageViewModel.cs
+++ EvansBioApp/EvansBioApp/ViewModels/ContactMePageViewModel.cs
@@ -3,18 +3,19 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Prism.Navigation;
 using Prism.Services;
 
 namespace EvansBioApp.ViewModels
 {
-	public class ContactMePageViewModel : BindableBase
+	public class ContactMePageViewModel : ViewModelBase
 	{
 	    private readonly IPageDialogService _pageDialog;
 	    private string _details;
 	    private string _subject;
 	    private string _email;
 
-	    public ContactMePageViewModel(IPageDialogService pageDialog)
+	    public ContactMePageViewModel(IPageDialogService pageDialog, INavigationService navigationService): base(navigationService) 
 	    {
 	        _pageDialog = pageDialog;
 	        SendCommand = new DelegateCommand(Send, CanSend);
@@ -51,7 +52,7 @@ namespace EvansBioApp.ViewModels
 	    {
 	        await _pageDialog.DisplayAlertAsync("Success!",
 	            "Thank you for your message!\nWe have received your message and will reply as soon as we can.", "OK");
-
+	        await NavigationService.GoBackAsync();
 	    }
 
 	    public DelegateCommand SendCommand { get; set; }
