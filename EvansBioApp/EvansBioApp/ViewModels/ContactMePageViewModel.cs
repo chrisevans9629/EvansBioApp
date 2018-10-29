b2079ba44219e5bf1dd50280e496819b32d23743
Chris Evans <Cevans@peerlessproducts.com>
2018-10-29 17:42:27 -0500

    added more contact me page validation

--- EvansBioApp/EvansBioApp/ViewModels/ContactMePageViewModel.cs
+++ EvansBioApp/EvansBioApp/ViewModels/ContactMePageViewModel.cs
@@ -14,6 +14,8 @@ namespace EvansBioApp.ViewModels
 	    private string _details;
 	    private string _subject;
 	    private string _email;
+	    private string _errorMessage;
+	    private bool _invalid;
 
 	    public ContactMePageViewModel(IPageDialogService pageDialog, INavigationService navigationService): base(navigationService) 
 	    {
@@ -22,33 +24,65 @@ namespace EvansBioApp.ViewModels
 	        SendCommand.ObservesProperty(() => Email);
 	        SendCommand.ObservesProperty(() => Subject);
 	        SendCommand.ObservesProperty(() => Details);
+            Validate();
+	    }
+
+
+	    public string ErrorMessage
+	    {
+	        get => _errorMessage;
+	        set => SetProperty(ref _errorMessage,value);
+	    }
+
+	    public bool Invalid
+	    {
+	        get => _invalid;
+	        set => SetProperty(ref _invalid,value);
 	    }
 
 	    private bool CanSend()
 	    {
-	        return !string.IsNullOrWhiteSpace(Email) && !string.IsNullOrWhiteSpace(Details) &&
-	               !string.IsNullOrWhiteSpace(Subject);
+	        Invalid = !(!string.IsNullOrWhiteSpace(Email) && !string.IsNullOrWhiteSpace(Details) &&
+	               !string.IsNullOrWhiteSpace(Subject));
+	        return !Invalid;
 	    }
 
 	    public string Email
 	    {
 	        get => _email;
-	        set => SetProperty(ref _email,value);
+	        set => SetProperty(ref _email,value, Validate);
 	    }
 
 	    public string Details
 	    {
 	        get => _details;
-	        set => SetProperty(ref _details,value);
+	        set => SetProperty(ref _details,value, Validate);
 	    }
 
 	    public string Subject
 	    {
 	        get => _subject;
-	        set => SetProperty(ref _subject,value);
+	        set => base.SetProperty(ref _subject, value, Validate);
 	    }
 
-	    private async void Send()
+        private void Validate()
+        {
+            ErrorMessage = "";
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                ErrorMessage += "email cannot be null\n";
+            }
+            if (string.IsNullOrWhiteSpace(Details))
+            {
+                ErrorMessage += "details cannot be null\n";
+            }
+            if (string.IsNullOrWhiteSpace(Subject))
+            {
+                ErrorMessage += "subject cannot be null\n";
+            }
+        }
+
+        private async void Send()
 	    {
 	        await _pageDialog.DisplayAlertAsync("Success!",
 	            "Thank you for your message!\nWe have received your message and will reply as soon as we can.", "OK");
